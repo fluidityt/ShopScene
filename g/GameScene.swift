4a73e04e8ef94b5f19ab40b745d0ea5879a0fb73
fluidityt <hackbook17@justins-macbook-pro.local>
2017-06-06 02:02:14 -0400

    kk

--- g/GameScene.swift
+++ g/GameScene.swift
@@ -1,40 +1,35 @@
-//
-//  GameScene.swift
-//  g
-//
-//  Created by justin fluidity on 6/5/17.
-//  Copyright © 2017 justin fluidity. All rights reserved.
-//
 
 import SpriteKit
-import GameplayKit
 
+// Placeholder texture since I'm too lazy to add actual art:
 let tex = SKTexture(noiseWithSmoothness: 22, size: CGSize(width: 50, height: 50), grayscale: false)
 
 typealias Price = Int
 
 /// The items that are for sale in our shop:
 struct Costume {
-
-  static var masterList: [Costume] = []
   
+  static var allCostumes: [Costume] = []
+
   let name: String
   let texture: SKTexture
   let price: Price
   
   // This init simply adds all costumes to a master list for easy sorting later on.
   init(name: String, texture: SKTexture, price: Price) { self.name = name; self.texture = texture; self.price = price
-    Costume.masterList.append(self)
+    Costume.allCostumes.append(self)
   }
   
   // Hard-code any new costumes you create here (this is a "master list" of costumes):
   // (make sure all of your costumes have a unique name, or the program will not work properly)
-  static let
-  gray  = Costume(name: "Gray Shirt",  texture: tex/*SKTexture(imageNamed: "grayshirt")*/,  price:  0), // Gray shirt is our default shirt
-  red   = Costume(name: "Red Shirt",   texture: tex/*SKTexture(imageNamed: "redshirt")*/,   price: 25),
-  blue  = Costume(name: "Blue Shirt",  texture: tex/*SKTexture(imageNamed: "blueshirt")*/,  price: 50),
-  green = Costume(name: "Green Shirt", texture: tex/*SKTexture(imageNamed: "greenshirt")*/, price: 75)
+  static let list = (
+    gray:  Costume(name: "Gray Shirt",  texture: tex/*SKTexture(imageNamed: "grayshirt")*/,  price:  0),
+    red:   Costume(name: "Red Shirt",   texture: tex/*SKTexture(imageNamed: "redshirt")*/,   price: 25),
+    blue:  Costume(name: "Blue Shirt",  texture: tex/*SKTexture(imageNamed: "blueshirt")*/,  price: 50),
+    green: Costume(name: "Green Shirt", texture: tex/*SKTexture(imageNamed: "greenshirt")*/, price: 75)
+  )
   
+  static let defaultCostume = list.gray
 };
 
 // The reason why you need unique names:
@@ -65,11 +60,13 @@ class Player: SKSpriteNode {
   required init?(coder aDecoder: NSCoder) { fatalError() }
 };
 
-// Helpers:
 class GameScene: SKScene {
-
-  let player = Player(costume: Costume.gray)
-    
+  
+  let player = Player(costume: Costume.defaultCostume)
+  
+  override func didMove(to view: SKView) {
+    // addChild(player)
+  }
   override func mouseDown(with event: NSEvent) {
     view!.presentScene(ShopScene(previousGameScene: self))
   }
--- g/ShopStuff.swift
+++ g/ShopStuff.swift
@@ -1,8 +1,16 @@
-//
-//  ShopStuff.swift
-
 import SpriteKit
 
+// Helpers:
+extension SKScene {
+  func addChildren(_ nodes: [SKNode]) { for node in nodes { addChild(node) } }
+}
+fileprivate func halfHeight(_ node: SKNode) -> CGFloat { return node.frame.size.height/2 }
+fileprivate func halfWidth (_ node: SKNode) -> CGFloat { return node.frame.size.height/2 }
+fileprivate func costumeSpriteNode(costume: Costume) -> SKSpriteNode {
+  let csn = SKSpriteNode(texture: costume.texture)
+  csn.name = costume.name
+  return csn
+}
 
 /// Our model class to be used inside of our ShopScene:
 class Shop {
@@ -14,10 +22,10 @@ class Shop {
   
   // Add each costume you want to be available for purchase to the player at this time:
   // (The green shirt wont become available until the player has cleared 2 levels)
-  var availableCostumes: [Costume] = [Costume.red, Costume.blue]
+  var availableCostumes: [Costume] = [Costume.list.red, Costume.list.blue]
   
   // Keeps track of sold costumes:
-  var soldCostumes: [Costume] = [Costume.gray]                // Gray shirt is our default shirt
+  var soldCostumes: [Costume] = [Costume.defaultCostume]
   
   func canBuyCostume(_ costume: Costume) -> Bool {
     
@@ -58,16 +66,12 @@ class Shop {
 /// The scene in which we can interact with our shop and player:
 class ShopScene: SKScene {
   
-  // Helpers:
-  func addChildren(_ nodes: [SKNode]) { for node in nodes { addChild(node) } }
-  func halfHeight(_ node: SKNode) -> CGFloat { return node.frame.size.height/2 }
-  func halfWidth (_ node: SKNode) -> CGFloat { return node.frame.size.height/2 }
-
   // Properties:
   private lazy var shop: Shop = Shop(shopScene: self)
   
   let previousGameScene: GameScene
   
+  // The player is actually still in the other scene, not this one
   var player: Player { return self.previousGameScene.player }
   
   // Changes as you click around the screen
@@ -78,25 +82,29 @@ class ShopScene: SKScene {
   private let
   playerAvatarNode = SKSpriteNode(),
   buyNode  = SKLabelNode(),
-  exitNode = SKSpriteNode()
+  exitNode = SKLabelNode()
   
+  // Node names are used for determining if buttons are pressed, costumes are touched..
   private func setUpNodes() {
     
-    // Buy node:
-    buyNode.text = "Buy Costume"
-    buyNode.position.y = frame.minY + halfHeight(buyNode)
+    setupBuyNode: do {
+      buyNode.text = "Buy Costume"
+      buyNode.fontName = "Chalkduster"
+      buyNode.position.y = frame.minY + halfHeight(buyNode)
+      buyNode.name = "buy"
+    }
     
-    // Costume node:
-    do {
-      for costume in Costume.masterList {
-        costumeNodes.append(SKSpriteNode(texture: costume.texture))
+    setupCostumeNodes: do {
+      guard Costume.allCostumes.count > 1 else { fatalError("must have at least two costumes (for while loop)") }
+      
+      for costume in Costume.allCostumes {
+        costumeNodes.append(costumeSpriteNode(costume: costume))
       }
-      guard costumeNodes.count == Costume.masterList.count else { fatalError("duplicate nodes found") }
+      guard costumeNodes.count == Costume.allCostumes.count else { fatalError("duplicate nodes found") }
       
       costumeNodes[0].position.x = frame.minX + halfWidth(costumeNodes[0])
-      print(Costume.masterList.count)
       var counter = 1
-      var finalIndex = costumeNodes.count - 1
+      let finalIndex = costumeNodes.count - 1
       
       // Place nodes from left to right:
       while counter <= finalIndex {
@@ -108,12 +116,22 @@ class ShopScene: SKScene {
       }
     }
     
-    // Player Avatar Node:
-    guard let texture = player.texture else { fatalError("player has no texture!! Is .wear()ing costume?") }
-    playerAvatarNode.texture = texture
-    playerAvatarNode.size = texture.size()
+    setupPlayerAvatarNode: do {
+      guard let texture = player.texture else { fatalError("player has no texture!! Is .wear()ing costume?") }
+      playerAvatarNode.texture = texture
+      playerAvatarNode.size = texture.size()
+    }
     
+    setupExitNode: do {
+      exitNode.text = "Leave Shop"
+      exitNode.fontName = "Chalkduster"
+      exitNode.position.y = frame.maxY - halfHeight(buyNode)
+    }
+    
+    // Add them to scene:
     addChildren(costumeNodes)
+    addChildren([selectedNode, playerAvatarNode, buyNode, exitNode])
+    
   }
   
   // Init:
@@ -127,16 +145,14 @@ class ShopScene: SKScene {
   
   deinit { print("shopscene: if you don't see this message when exiting shop then you have a retain cycle") }
   
-  // UI:
-  
-  
-  
   // Game loop:
   override func didMove(to view: SKView) {
-   anchorPoint = CGPoint(x: 0.5, y: 0.5)
+    anchorPoint = CGPoint(x: 0.5, y: 0.5)
     setUpNodes()
   }
   
+  // I'm choosing to have the buttons activated by searching for name here. You can also
+  // subclass a node and have them do actions on their own when clicked.
   override func mouseDown(with event: NSEvent) {
     shop.exitShop()
   }
